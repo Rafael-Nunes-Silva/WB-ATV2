70331801214f012ab58d077897f60d0053cd7c83
Rafael-Nunes-Silva <rafaelnunesilva20@gmail.com>
2023-11-01 09:22:53 -0300

    Inicio da barra de navegação no header

--- world-beauty/src/index.tsx
+++ world-beauty/src/index.tsx
@@ -1,15 +1,42 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Cadastro from "./pages/cadastro";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route path="cadastro">
+            <Route path="cliente" element={<Cadastro />} />
+            <Route path="produto" element={<App />} />
+            <Route path="servico" element={<App />} />
+          </Route>
+          <Route path="listagem" element={<App />}>
+            <Route path="cliente" element={<App />} />
+            <Route path="produto" element={<App />} />
+            <Route path="servico" element={<App />} />
+          </Route>
+          <Route path="atualizar" element={<App />}>
+            <Route path="cliente" element={<App />} />
+            <Route path="produto" element={<App />} />
+            <Route path="servico" element={<App />} />
+          </Route>
+          <Route path="deletar" element={<App />}>
+            <Route path="cliente" element={<App />} />
+            <Route path="produto" element={<App />} />
+            <Route path="servico" element={<App />} />
+          </Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
