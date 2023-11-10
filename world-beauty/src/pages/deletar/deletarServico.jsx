b03eacf55b2e54c6a96dba6108971d2b57d2ab90
Rafael-Nunes-Silva <rafaelnunesilva20@gmail.com>
2023-11-10 10:37:02 -0300

    Deletar produtos e serviços

--- world-beauty/src/pages/deletar/deletarServico.jsx
+++ world-beauty/src/pages/deletar/deletarServico.jsx
@@ -1,6 +1,36 @@
+import { useState } from "react";
+import { APIServico } from "../../WB-ATV1/out/Interface/APIReact";
+import Select from "../../components/select";
+
 export default function DeletarServico() {
+    const [nomeServSelecionado, setNomeServSelecionado] = useState("");
+
+    const HandleSubmit = function (event) {
+        APIServico.Deletar(nomeServSelecionado);
+    }
+
     return (
         <div>
+            <form onSubmit={HandleSubmit} className="form-cadastro">
+                <div className="form-horizontal-div">
+                    <div className="form-vertical-div">
+                        <label for="servico">Serviço</label>
+                        <Select
+                            name="servico"
+                            options={APIServico.GetServicos().map(
+                                (s) => ({ name: s.nome, value: s.valor })
+                            )}
+                            onChange={(event) => {
+                                const selecionado = event.target.options[event.target.selectedIndex];
+                                setNomeServSelecionado(selecionado.innerText);
+                            }}
+                        />
+                    </div>
+                </div>
+                <div className="form-horizontal-div">
+                    <button type="submit" className="form-submit">Deletar</button>
+                </div>
+            </form>
         </div>
     );
 }
